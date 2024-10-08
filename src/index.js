e5d057d8b3e22fd5edd557fe59e2bfe43c3e84ad
Yolanda Mejane <mysivuyile@gmail.com>
2024-10-08 17:25:04 +0200

    "did Week 7 homework update"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,88 @@
+const iconMappings = {
+  "clear sky": {
+    day: "clear-sky-day.png",
+    night: "clear-sky-night.png",
+  },
+  "few clouds": {
+    day: "few-clouds-day.png",
+    night: "few-clouds-night.png",
+  },
+  "scattered clouds": {
+    day: "scattered-clouds-day.png",
+    night: "scattered-clouds-night.png",
+  },
+  "broken clouds": {
+    day: "broken-clouds-day.png",
+    night: "broken-clouds-night.png",
+  },
+  "overcast clouds": {
+    day: "broken-clouds-day.png",
+    night: "broken-clouds-night.png",
+  },
+  "mist": {
+    day: "mist-day.png",
+    night: "mist-night.png",
+  },
+  "light rain": {
+    day: "rain-day.png",
+    night: "rain-night.png",
+  },
+  "rain": {
+    day: "rain-day.png",
+    night: "rain-night.png",
+  },
+  "shower rain": {
+    day: "shower-rain-day.png",
+    night: "shower-rain-night.png",
+  },
+  "thunderstorm": {
+    day: "thunderstorm-day.png",
+    night: "thunderstorm-night.png",
+  },
+  "snow": {
+    day: "snow-day.png",
+    night: "snow-night.png",
+  },
+  "haze": {
+    day: "mist-day.png",
+    night: "mist-night.png",
+  }
+};
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
+
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+  humidityElement.innerHTML = `Humidity: <strong>${humidity}</strong>`;
+
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let windSpeedMps = response.data.wind.speed;
+  let windDirection = response.data.wind.degree;
+  let windSpeedKmh = (windSpeedMps * 3.6).toFixed(1);
+  windSpeedElement.innerHTML = `Wind: <strong>${windSpeedKmh} km/h</strong>`;
+
+  let weatherConditionElement = document.querySelector("#weather-condition");
+  let weatherCondition = response.data.condition.description;
+  weatherConditionElement.innerHTML = `Condition: ${weatherCondition}`;
+
+  let iconFileName;
+  let isDayTime = response.data.time;
+
+  if (isDayTime) {
+    iconFileName = iconMappings[weatherCondition.toLowerCase()]?.day || "default-icon.png";
+  } else {
+    iconFileName = iconMappings[weatherCondition.toLowerCase()]?.night || "default-icon.png";
+  }
+
+  let weatherIconElement = document.querySelector("#weather-icon");
+  let iconUrl = `src/assets/${iconFileName}`;
+  weatherIconElement.src = iconUrl;
+  weatherIconElement.alt = weatherCondition;
 }
 
 function search(event) {
@@ -11,7 +90,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "200e0d6tebb283fa19534eeofca3556f";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
@@ -47,7 +126,6 @@ function formatDate(date) {
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
-let currentDateELement = document.querySelector("#current-date");
+let currentDateElement = document.querySelector("#current-date");
 let currentDate = new Date();
-
-currentDateELement.innerHTML = formatDate(currentDate);
+currentDateElement.innerHTML = formatDate(currentDate);
